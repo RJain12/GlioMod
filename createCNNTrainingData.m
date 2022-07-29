f12e9848cc91dbe3bd6f735c48dd5b202d6314b7
Rishab Jain <397630@bsd48.org>
2022-07-29 13:59:10 -0400

    Update createCNNTrainingData.m

--- createCNNTrainingData.m
+++ createCNNTrainingData.m
@@ -1,15 +1,35 @@
 % Initialize empty XTrain and YTrain
 dim = 64;
-tot = 5000;
+tot = 5000; % num of total training samples
+tott = 1000; % num of total testing samples
+numPatients = 5; % num of training patients
 TTrain = zeros(dim,dim,dim,tot);
-%YTrain = cell(5400,1);
+TTest = zeros(dim,dim,dim,tot/numPatients);
 
 % Create autoencoder to bring image into latentspace
 
 g = 1;
 for k = 1:10
     empty = zeros(dim,dim,dim);
-    for n = 1 : 5
+     for n = 1 : numPatients
+         for m = 1 : size(dayStruct{n,k},3)
+             currImage = dayStruct{n,k}(:,:,m);
+             copy = empty;
+             for l = 1 : length(currImage)
+                 currRow = currImage(l,:);
+                 copy(round(currRow(1) * dim),round(currRow(2) * dim), round(dim * currRow(3))) = currRow(4); % * 255
+             end
+             TTrain(:,:,:,g) = copy;
+             g = g+1;
+         end
+     end
+ end
+
+
+g = 1;
+for k = 1:10
+    empty = zeros(dim,dim,dim);
+    for n = numPatients : numPatients + 1
         for m = 1 : size(dayStruct{n,k},3)
             currImage = dayStruct{n,k}(:,:,m);
             copy = empty;
@@ -17,14 +37,18 @@ for k = 1:10
                 currRow = currImage(l,:);
                 copy(round(currRow(1) * dim),round(currRow(2) * dim), round(dim * currRow(3))) = currRow(4); % * 255
             end
-            TTrain(:,:,:,g) = copy;
+            TTest(:,:,:,g) = copy;
             g = g+1;
         end
     end
 end
 
+
 XTrain = TTrain(:,:,:,1:tot - tot/10);
 YTrain = TTrain(:,:,:,tot/10 + 1:tot);
+
+XTest = TTest(:,:,:,1:tott - tott/10);
+YTest = TTest(:,:,:,tott/10 + 1:tott);
 
 xds = arrayDatastore(XTrain,IterationDimension=4);
 yds = arrayDatastore(YTrain,IterationDimension=4);
